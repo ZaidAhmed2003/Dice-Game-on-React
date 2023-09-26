17c47dcbb9620233f8dffb139d89da64bce8d7c4
Zaid Ahmed <zaidahmed2345@gmail.com>
2023-09-26 17:39:57 +0500

    fixes

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -9,7 +9,7 @@ const Rules = () => {
         <p>Click on dice image</p>
         <p>
           after click on dice if selected number is equal to dice number you
-          will get same point as dice{" "}
+          will get same point as dice.
         </p>
         <p>if you get wrong guess then 2 point will be dedcuted </p>
       </div>
